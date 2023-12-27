3e55602eab278cf14a429b1095f226ed63e52ddd
syed rafay <sp22bsse0054@maju.edu.pk>
2023-12-27 14:33:20 +0500

    check it my boy

--- student (1).sql
+++ student (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 27, 2023 at 09:26 AM
+-- Generation Time: Dec 27, 2023 at 10:31 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `grading`
+--
+
+CREATE TABLE `grading` (
+  `id` int(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `course` varchar(50) NOT NULL,
+  `result` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `registration`
 --
 
@@ -34,6 +47,16 @@ CREATE TABLE `registration` (
   `Father name` varchar(30) NOT NULL,
   `Email` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`id`, `Name`, `Phone no`, `Father name`, `Email`) VALUES
+(1, 'syed rafay', 312287132, 'sohail arshad ', 'syedrafay507@gmail.c'),
+(2, 'farhan ifti', 329807654, 'iftikhar ', 'farhan@gmail.com'),
+(3, 'musab ', 356786464, 'akbar ali ', 'musab@gmail.com'),
+(4, 'ammar ', 345362784, 'nadeem ', 'ammar@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -56,6 +79,12 @@ CREATE TABLE `time_table` (
 --
 
 --
+-- Indexes for table `grading`
+--
+ALTER TABLE `grading`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `registration`
 --
 ALTER TABLE `registration`
@@ -66,70 +95,16 @@ ALTER TABLE `registration`
 --
 
 --
--- AUTO_INCREMENT for table `registration`
---
-ALTER TABLE `registration`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 27, 2023 at 09:57 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `test`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `grading`
---
-
-CREATE TABLE `grading` (
-  `S.id` int(34) NOT NULL,
-  `S.name` varchar(50) NOT NULL,
-  `Course` varchar(50) NOT NULL,
-  `Result` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `grading`
---
-ALTER TABLE `grading`
-  ADD PRIMARY KEY (`S.id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
 -- AUTO_INCREMENT for table `grading`
 --
 ALTER TABLE `grading`
-  MODIFY `S.id` int(34) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
