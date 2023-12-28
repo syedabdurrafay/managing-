9bf926e48844c37934f2be607326202ad20d7b29
syed rafay <sp22bsse0054@maju.edu.pk>
2023-12-28 19:31:48 +0500

    data changed

--- student (3).sql
+++ student (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2023 at 12:22 PM
+-- Generation Time: Dec 28, 2023 at 03:30 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -40,10 +40,12 @@ CREATE TABLE `grading` (
 --
 
 INSERT INTO `grading` (`S.id`, `S.Name`, `Course`, `Grade`, `Result`) VALUES
-(1, 'Farhan Iftikhar', 'English', 'A', 'Pass'),
-(2, 'Abdul Rafay ', 'Software Requirement Engineering', 'B', 'Pass'),
-(3, 'Muhammad Musab', 'Islamiat', 'B', 'Pass'),
-(4, 'Noman Khan', 'Pakistan Studies', 'C', 'Pass');
+(1, 'syed rafay', 'English/software requirement/math/pst/isl', 'A,B,A,C,A', 'Pass'),
+(2, 'muhammad musab ', 'english/Software Requirement /math/pst/islamiat', 'B,A,C,A,A', 'Pass'),
+(3, 'farhan iftikhar', 'english/Software Requirement /math/pst/islamiat', 'B,B,C,A,A', 'Pass'),
+(4, 'ammar nadeem ', 'english/Software Requirement /math/pst/islamiat', 'C,C,A,A,A', 'Pass'),
+(5, 'ali raza ', 'english/Software Requirement /math/pst/islamiat', 'B,A,C,A,A', 'pass'),
+(6, 'umar', 'english/Software Requirement /math/pst/islamiat', 'A,B,A,C,C', 'pass');
 
 -- --------------------------------------------------------
 
@@ -52,22 +54,27 @@ INSERT INTO `grading` (`S.id`, `S.Name`, `Course`, `Grade`, `Result`) VALUES
 --
 
 CREATE TABLE `registration` (
-  `id` int(30) NOT NULL,
-  `Name` varchar(30) NOT NULL,
-  `Phone no` int(30) NOT NULL,
-  `Father name` varchar(30) NOT NULL,
-  `Email` varchar(20) NOT NULL
+  `studentID` int(50) NOT NULL,
+  `studentName` varchar(50) NOT NULL,
+  `fatherName` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phoneNO` int(50) NOT NULL,
+  `department` varchar(50) NOT NULL,
+  `semester` varchar(50) NOT NULL,
+  `staringYear` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `registration`
 --
 
-INSERT INTO `registration` (`id`, `Name`, `Phone no`, `Father name`, `Email`) VALUES
-(6, '', 0, '', ''),
-(7, '', 0, '', ''),
-(8, '', 0, '', ''),
-(9, '[value-2]', 0, '[value-4]', '[value-5]');
+INSERT INTO `registration` (`studentID`, `studentName`, `fatherName`, `email`, `phoneNO`, `department`, `semester`, `staringYear`) VALUES
+(1, 'syed rafay', 'sohail arshad ', 'syedrafay@gmail.com', 312287132, 'software engineering', '4', '2022'),
+(2, 'muhammad musab', 'akbar ali ', 'musab@gmail.com', 344451651, 'software engineering', '4', '2022'),
+(3, 'farhan iftikhar', 'iftikhar hussain', 'farhan@gmail.com', 322175334, 'software engineering', '4', '2022'),
+(4, 'ammar nadeem ', 'nadeem arshad ', 'ammar@gmail.com', 366716633, 'software engineenring', '4', '2022'),
+(5, 'ali raza', 'sohail arshad ', 'ali@gmail.com', 322175335, 'business', '5', '2021'),
+(6, 'umar ', 'zubair', 'umar@gmail.com', 398606343, 'science', '4', '2022');
 
 -- --------------------------------------------------------
 
@@ -109,7 +116,7 @@ ALTER TABLE `grading`
 -- Indexes for table `registration`
 --
 ALTER TABLE `registration`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`studentID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -119,13 +126,13 @@ ALTER TABLE `registration`
 -- AUTO_INCREMENT for table `grading`
 --
 ALTER TABLE `grading`
-  MODIFY `S.id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `S.id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `studentID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
